e57f6bfc6d561b754fc2261ea8e84ea6280227da
trancuong763 <tranvancuong.9763@gmail.com>
2023-04-14 14:13:24 +0700

    feat: add new readme

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
     .setTitle('Coinmap Hiring API')
     .setDescription('The Coinmap Hiring API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
